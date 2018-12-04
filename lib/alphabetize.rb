0973f1593d68ebf64a1ffbf13eb112b5356c92a3
Dania Herrera <danherrera0@gmail.com>
2018-12-04 22:00:48 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,8 @@
+
 ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 
 def alphabetize(arr)
   arr.sort_by { |a| a.split("").map{ |character| ESPERANTO_ALPHABET.index(character) } }
+  binding.pry
 end
